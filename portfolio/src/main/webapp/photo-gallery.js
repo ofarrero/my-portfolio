64aec460c4b07107b93c6ff729541a7e57dbf82e
ofarrero <ro.ofarrell1@gmail.com>
2020-07-02 09:33:57 +0000

    Add photo gallery slideshow functionality

---
+++ portfolio/src/main/webapp/photo-gallery.js
@@ -0,0 +1,48 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls called in html onclick
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myPhotos");
+  let markers = document.getElementsByClassName("marker");
+
+  //go to first picture at end of slide deck
+  if (n > slides.length) {slideIndex = 1}
+
+  //got to last picture from the start of deck
+  if (n < 1) {slideIndex = slides.length}
+
+  //ensure no pictures are displayed
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
+  //ensure no markers are active
+  for (i = 0; i < markers.length; i++) {
+      markers[i].className = markers[i].className.replace(" active", "");
+  }
+
+  // make correct picture and marker active (take away one as slideIndex starts at 1)
+  slides[slideIndex-1].style.display = "block";
+  markers[slideIndex-1].className += " active";
+}
