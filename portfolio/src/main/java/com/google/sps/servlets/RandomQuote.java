d6f4c06102cdea04d3e0cc99fdb29864a2cd58d7
Róisín O'Farrell <66687614+ofarrero@users.noreply.github.com>
2020-07-08 16:05:17 +0100

    Fix Comments
    Comment describing codes overall function was outdated

--- portfolio/src/main/java/com/google/sps/servlets/RandomQuote.java
+++ portfolio/src/main/java/com/google/sps/servlets/RandomQuote.java
@@ -12,49 +12,36 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.servlets;
+/**
+ * Fetches a random quote from the server and adds it to the DOM.
+ */
+function getRandomQuote() {
+  console.log('Fetching a random quote.');
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+  const responsePromise = fetch('/random-quote');
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/random-quote")
-public class DataServlet extends HttpServlet {
-  private List<String> quotes;
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
 
-  @Override
-  public void init() {
-    quotes = new ArrayList<>();
-    quotes.add("I was taught the way of progress is neither swift nor easy. - Marie Curie");
-    quotes.add(
-        "I'd like to hear less talk about men and women and more talk about citizens."
-        + " - Marjory Stoneman Douglas");
-    quotes.add(
-        "There is no joy more intense than that of coming upon a fact that cannot "
-        + "be understood in terms of currently accepted ideas. - Cecilia Payne-Gaposchkin");
-    quotes.add("When you love science all you really want to do is keep working. - Maria Goeppert-Mayer");
-    quotes.add("People are allergic to change you have to get out and sell the idea - Grace Hopper");
-    quotes.add("Above all don't fear difficult moments. The best comes from them. - Rita Levi-Montalcini");
-    quotes.add(
-        "[My Father] made me understand that I must make my own decisions, mold my own character, "
-            + "think my own thoughts.  - Hedy Lamar");
-    quotes.add(
-        "We must believe in ourselves or no one else will believe in us; We must match our aspirations "
-            + "with the competence, courage and determination to succeed. - Rosalyn Yalow");
-    quotes.add("Science and everyday life cannot and should not be seperated. - Rosalind Franklin");
-  }
+/**
+ * Handles response by converting it to text and passing the result to
+ * addQuoteToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String quote = quotes.get((int) (Math.random() * quotes.size()));
+  const textPromise = response.text();
 
-    response.setContentType("text/html;");
-    response.getWriter().println(quote);
-  }
+  // When the response is converted to text, pass the result into the
+  // addQuoteToDom() function.
+  textPromise.then(addQuoteToDom);
+}
 
+/** Adds a random quote to the DOM. */
+function addQuoteToDom(quote) {
+  console.log('Adding quote to dom: ' + quote);
+
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
